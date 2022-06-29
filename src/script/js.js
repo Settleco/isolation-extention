d5ae6cf2b2cbf46c991c62c284091b682be6bed7
Mert Doğu <mertdoguonder@protonmail.com>
2022-06-29 19:31:07 +0300

    add: api

--- src/script/js.js
+++ src/script/js.js
@@ -6,21 +6,14 @@
 const name = "Mert"; // Setting Name
 
 let date = new Date();
-let day = date.getDate();
-let month = date.getMonth() + 1;
-let year = date.getFullYear();
 let hour = date.getHours();
 let minute = date.getMinutes();
 
-const dateElement = document.getElementById("date");
 const nameElement = document.getElementById("name");
 const hourElement = document.getElementById("hours");
 
 setInterval(() => {
     date = new Date();
-    day = date.getDate();
-    month = date.getMonth() + 1;
-    year = date.getFullYear();
     hour = date.getHours();
     minute = date.getMinutes();
 } , 1000);
@@ -54,11 +47,9 @@ function stringifyMonth (month) {
     }
 }
 
-const dateString = `${day} ${stringifyMonth(month)}, ${year}`;
-dateElement.innerHTML = dateString;
 hourElement.innerHTML = `${hour}:${minute}`;
 
-// --------------------- Greetning Msg --------------------- 
+// --------------------- START ---------------------
 const welcomeMessageElement = document.getElementById("welcome-message");
 if (hour < 12) { 
     welcomeMessageElement.innerHTML = "Günaydın, " + name + ".";
@@ -68,18 +59,80 @@ if (hour < 12) {
 else {
     welcomeMessageElement.innerHTML = "İyi akşamlar, " + name;
 }
-// --------------------- Greetning Msg --------------------- 
+// --------------------- END ---------------------
 
-//
 
-let headers = new Headers();
-headers.append('Access-Control-Allow-Origin', 'http://localhost:8080');
-headers.append('Access-Control-Allow-Credentials', 'true');
+// --------------------- START ---------------------
 
-fetch('https://api.genelpara.com/embed/kripto.json',{
-    method: 'GET',
-    mode: 'cors',
-    credentials: 'include'
+const btc = document.getElementById("btc");
+const btcchange = document.getElementById("btcchange");
+fetch('https://api.coingecko.com/api/v3/coins/bitcoin?tickers=false&community_data=false&developer_data=false&sparkline=false',{
+    headers : {
+        'Access-Control-Allow-Origin': '*'
+    },
   })
   .then(response => response.json())
-  .then(data => console.log(data))
+  .then(data => {
+    btc.innerHTML = Number.parseFloat(data.market_data.current_price.usd).toFixed(2)
+    btcchange.innerHTML = Number.parseFloat(data.market_data.price_change_percentage_24h).toFixed(2)
+  })
+
+const eth = document.getElementById("eth");
+const ethchange = document.getElementById("ethchange");
+fetch('https://api.coingecko.com/api/v3/coins/ethereum?tickers=false&community_data=false&developer_data=false&sparkline=false',{
+    headers : {
+        'Access-Control-Allow-Origin': '*'
+    },
+  })
+  .then(response => response.json())
+  .then(data => {
+    eth.innerHTML = Number.parseFloat(data.market_data.current_price.usd).toFixed(2);
+    ethchange.innerHTML = Number.parseFloat(data.market_data.price_change_percentage_24h).toFixed(2);
+  })
+
+  const ltc = document.getElementById("ltc");
+  const ltcchange = document.getElementById("ltcchange");
+  fetch('https://api.coingecko.com/api/v3/coins/litecoin?tickers=false&community_data=false&developer_data=false&sparkline=false',{
+    headers : {
+        'Access-Control-Allow-Origin': '*'
+    },
+    })
+    .then(response => response.json())
+    .then(data => {
+        ltc.innerHTML = Number.parseFloat(data.market_data.current_price.usd).toFixed(2);
+        ltcchange.innerHTML = Number.parseFloat(data.market_data.price_change_percentage_24h).toFixed(2);
+    })
+
+    const doge = document.getElementById("doge");
+    const dogechange = document.getElementById("dogechange");
+    fetch('https://api.coingecko.com/api/v3/coins/dogecoin',{
+        headers : {
+            'Access-Control-Allow-Origin': '*'
+        },
+      })
+      .then(response => response.json())
+      .then(data => {
+        doge.innerHTML = Number.parseFloat(data.market_data.current_price.usd).toFixed(4);
+        dogechange.innerHTML = Number.parseFloat(data.market_data.price_change_percentage_24h).toFixed(2);
+      })
+// --------------------- END ---------------------
+const spotifytitle = document.getElementById("spotifytitle");
+const spotifyartist = document.getElementById("spotifyartist"); 
+
+function spotifyData(token){
+    fetch('https://api.spotify.com/v1/me/player/currently-playing?limit=1',{
+    headers : {
+        'Authorization': token
+    }
+  })
+  .then(response => response.json())
+  .then(data => {
+    spotifytitle.innerHTML = data.item.name;
+    spotifyartist.innerHTML = data.item.album.artists[0].name;
+    console.log(data);
+  })
+}
+
+setInterval( () => {
+    spotifyData("Bearer BQDSYpAb5t29Y7ul7QymB5j1W3-zCR2Qy7Fb3LiTbFKJem7fDUS4wGX9G9UZmJz5hrYXspCDC2thjzYA_AlV0P6K3R5tT5z9eZfgGvgzXYg84h5sutkoam8Gi1XJS9eR8GyB8ilA_hA1W29AV9vpxjoMnylBB_stUZFeYVPUKbj_2eyiMPNzsgtRYMaHxly3UDWAbnx72qI")
+},5000)
